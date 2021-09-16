85abd3d4dd8707146d5da478edae4eb4b82eb597
Jesse James <jessejames@Jesses-MBP.fios-router.home>
2021-09-15 21:44:00 -0400

    Initial Commit

---
+++ js/mainpage.js
@@ -0,0 +1,46 @@
+
+anime({
+    targets: 'p',
+    translateX: 250,
+    rotate: '1turn',
+    duration: 800
+  });
+
+//Main Page Fade Cookie
+// function changeBackground() {
+//     let backgroundImage = document.querySelector("image-mask-overlay")
+//     // window.sessionStorage.setItem('color', "black");
+//     // console.log(setItem);
+//     if (sessionStorage.getItem('color')) {
+//         backgroundImage === window.sessionStorage.getItem('color'); 
+//     }else{
+//         backgroundImage ===  "black";
+//         window.sessionStorage.setItem('color', "black");
+//     }
+// }
+
+// changeBackground();
+
+// // console.log(changeBackground());
+
+// // function createItem() {
+// //     // let content = document.querySelector('#image-mask-overlay');
+// //     sessionStorage.setItem("bgImage", element.style.backgroundColor = "black");
+// // }
+
+// function changeBackground() {
+//     if (sessionStorage.getItem('color')) {
+//         document.body.style.backgroundColor = "black";
+//     }else{
+//         document.body.style.backgroundColor =  "black";
+//         sessionStorage.setItem('color', "black");
+//     }
+// }
+
+// // then you'll need to call your function on page load
+// changeBackground();
+
+// // function setBackgroundColor() {
+    
+// // }
+
---
+++ js/splashpage.js
@@ -0,0 +1,7 @@
+// Splash Page JS Section
+const seanceButton = document.querySelector("#seance-enter");
+seanceButton.addEventListener("click", seanceEnter);
+
+function seanceEnter() {
+  window.location.href = "mainpage.html";
+};
