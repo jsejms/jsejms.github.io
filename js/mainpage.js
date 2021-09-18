54dca948c7be4b0e30bbd58a7f0788d143563073
Jesse James <jessejames@Jesses-MBP.fios-router.home>
2021-09-18 17:35:33 -0400

    added a nav bar

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,46 +1,19 @@
-
-anime({
-    targets: 'p',
-    translateX: 250,
-    rotate: '1turn',
-    duration: 800
-  });
-
-//Main Page Fade Cookie
-// function changeBackground() {
-//     let backgroundImage = document.querySelector("image-mask-overlay")
-//     // window.sessionStorage.setItem('color', "black");
-//     // console.log(setItem);
-//     if (sessionStorage.getItem('color')) {
-//         backgroundImage === window.sessionStorage.getItem('color'); 
-//     }else{
-//         backgroundImage ===  "black";
-//         window.sessionStorage.setItem('color', "black");
-//     }
-// }
-
-// changeBackground();
-
-// // console.log(changeBackground());
-
-// // function createItem() {
-// //     // let content = document.querySelector('#image-mask-overlay');
-// //     sessionStorage.setItem("bgImage", element.style.backgroundColor = "black");
-// // }
-
-// function changeBackground() {
-//     if (sessionStorage.getItem('color')) {
-//         document.body.style.backgroundColor = "black";
-//     }else{
-//         document.body.style.backgroundColor =  "black";
-//         sessionStorage.setItem('color', "black");
-//     }
-// }
-
-// // then you'll need to call your function on page load
-// changeBackground();
-
-// // function setBackgroundColor() {
+// anime({
+//     targets: 'div',
+//     translateY: 150,
     
-// // }
+//   });
+//   anime();
 
+
+    // Options for customization, see full list below
+    var options = {
+        canvas: '.my-canvas',
+        text: 'text'
+    };
+
+    // Initialize a new instance, by default the pieces will be 'hidden'
+    var myPieces = new Pieces(options);
+
+    // Show pieces using default options. See the full list of available operations below.
+    myPieces.showPieces();
