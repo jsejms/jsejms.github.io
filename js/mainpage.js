da80fe27a4697889e1ee7735ef12c6e339a09c49
Jesse James <jessejames@Jesses-MBP.fios-router.home>
2021-09-19 14:56:28 -0400

    added in Pieces library

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,19 +1,45 @@
+var canvas = document.querySelector('.seance-canvas');
+// var image = document.querySelectorAll('.seance-image');
+var items = [];
+var seanceImage = new Image(600, 300);
+seanceImage.src = 'img/technoisthedevil.png';
+items.push({type: 'image', value: seanceImage});
+console.log(items);
+var imageOptions = {
+    canvas: canvas,
+    // items: items,
+    image: seanceImage,
+    x: 'centerAll',
+    y: 'centerAll',
+    // text: 'Livecast',
+    fontSize: 80,
+    backgroundColor: "#00000",
+    // padding: '15 20 10 20',
+    color: "#ffff",
+    ty: function () { return Pieces.random(-100, 100); },
+    piecesWidth: function () { return Pieces.random(20, 150); },
+    extraSpacing: { extraX: 100, extraY: 400 },
+    angle: 60,
+    animation: {
+        duration: function () { return Pieces.random(1500, 2000); },
+        easing: 'easeOutQuint'
+    },
+};
+console.log(seanceImage);
+
+
+// var piece = seanceImage
+// var ty = piece.ty;
 // anime({
-//     targets: 'div',
-//     translateY: 150,
-    
-//   });
-//   anime();
+//     targets: piece,
+//     ty: function () {
+//         return [{ value: ty + 10, duration: 1000 }, { value: ty - 10, duration: 2000 }, { value: ty, duration: 1000 }]
+//     },
+//     duration: 2000,
+//     easing: 'linear',
+//     loop: true
+// });
 
-
-    // Options for customization, see full list below
-    var options = {
-        canvas: '.my-canvas',
-        text: 'text'
-    };
-
-    // Initialize a new instance, by default the pieces will be 'hidden'
-    var myPieces = new Pieces(options);
-
-    // Show pieces using default options. See the full list of available operations below.
-    myPieces.showPieces();
+var myPieces = new Pieces(imageOptions);
+myPieces.showPieces();
+console.log(imageOptions);
