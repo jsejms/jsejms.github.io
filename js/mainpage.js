bbef5835bee7addf5359476dd88fc7bf6ef281ed
Jesse James <jessejames@Jesses-MBP.fios-router.home>
2021-09-20 01:48:37 -0400

    added in table

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,25 +1,33 @@
-var canvas = document.querySelector('.seance-canvas');
-// var image = document.querySelectorAll('.seance-image');
+var canvas = document.querySelector('.seance-canvas'), ctx = canvas.getContext("2d");
+canvas.width = canvas.getBoundingClientRect().width;
+canvas.height = canvas.getBoundingClientRect().height;
+    
+var seanceImage = new Image(500, 100);
 var items = [];
-var seanceImage = new Image(600, 300);
-seanceImage.src = 'img/technoisthedevil.png';
-items.push({type: 'image', value: seanceImage});
+var imagesReady = 0;
+seanceImage.onload = function () {
+    ctx.drawImage(seanceImage, 500, 100);
+};
+seanceImage.src = 'img/seanceghost2.png';
+items.push({ type: 'image', value: seanceImage });
 console.log(items);
+
 var imageOptions = {
     canvas: canvas,
-    // items: items,
-    image: seanceImage,
+    items: items,
+    // image: seanceImage,
     x: 'centerAll',
     y: 'centerAll',
-    // text: 'Livecast',
-    fontSize: 80,
-    backgroundColor: "#00000",
-    // padding: '15 20 10 20',
+    piecesSpacing: 6,
+    text: 'seance_shows',
+    fontSize: 45,
+    backgroundColor: "rgba(0, 0, 0, 0.9)",
     color: "#ffff",
-    ty: function () { return Pieces.random(-100, 100); },
+    ty: function () { return Pieces.random(-350, 300); },
     piecesWidth: function () { return Pieces.random(20, 150); },
-    extraSpacing: { extraX: 100, extraY: 400 },
+    extraSpacing: { extraX: 100, extraY: 200 },
     angle: 60,
+    debug: false,
     animation: {
         duration: function () { return Pieces.random(1500, 2000); },
         easing: 'easeOutQuint'
@@ -28,10 +36,11 @@ var imageOptions = {
 console.log(seanceImage);
 
 
-// var piece = seanceImage
+
+// var piece = items;
 // var ty = piece.ty;
 // anime({
-//     targets: piece,
+//     targets: items,
 //     ty: function () {
 //         return [{ value: ty + 10, duration: 1000 }, { value: ty - 10, duration: 2000 }, { value: ty, duration: 1000 }]
 //     },
@@ -40,6 +49,20 @@ console.log(seanceImage);
 //     loop: true
 // });
 
+
 var myPieces = new Pieces(imageOptions);
-myPieces.showPieces();
+
+myPieces.showPieces()
 console.log(imageOptions);
+
+anime({
+    targets: '.seance-event-section',
+    translateY: 90,
+    easing: 'easeOutQuart'
+});
+
+anime({
+    targets: 'div',
+    translateY: 50,
+    easing: 'easeOutQuart'
+});
